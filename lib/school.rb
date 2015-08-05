7a2726deb6f50c55d5ae86a780d75e1ddb160264
Tyler Machen <tylermachen@gmail.com>
2015-08-05 09:50:18 -0400

    complete School class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# Write code here
+class School
+  attr_accessor :roster; attr_reader :school
+
+  def initialize(school)
+    @school = school; @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster.include?(grade) ? @roster[grade] << name : @roster[grade] = [name]
+  end
+
+  def grade(grade)
+    @roster.select { |k| k == grade }[grade]
+  end
+
+  def sort
+    s = {}; @roster.sort_by { |k, v| s[k] = v.sort }; s
+  end
+end
